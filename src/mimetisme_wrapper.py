6072991683d1b905e9e6d6710c7a9d3a00e6abe4
Raphael <duchainer@gmail.com>
2020-04-23 17:14:37 -0400

    WIP pre test mimetisme stack

--- src/mimetisme_wrapper.py
+++ src/mimetisme_wrapper.py
@@ -1,50 +1,20 @@
 #! /usr/bin/env python3
 
 import rospy
-from moveit_commander import MoveGroupCommander
-from geometry_msgs.msg import Pose
 import sys
 import pipes
+from subprocess import Popen, PIPE
+from shlex import split
 
 class MimetismeWrapper(object):
     def __init__(self, group="RightArm"):
         rospy.init_node("MimetismeWrapper")
-#        self.pub = rospy.Publisher('blalaba', JointTrajectory, queue_size=10)
-#        self.rate = rospy.Rate(10)  # 10hz
-        self.group = MoveGroupCommander(group)
         print("Just initialised MimetismeWrapper")
 
+    #Call bash command equivalent "python openpifpafwebdemo | python pifpafToMoveit"
     def main_task(self):
-        while True:
-            line = sys.stdin.readline()  # Lis ligne dans le pipelin stdin
-#            line = regex.sub("", line)  # enlève les motifs de mot prédeterminés de la string par substitution
-
-            # si on a plus d'entrée
-            if not line:
-                break  # on sort de la boucle while (fin du programme)
-
-            # si on a que des espaces blanc
-            if line.isspace() or "[]" in line:
-                continue  # on passe à la prochaine itération
-
-            self.group.set_pose_target(userdata.target)
-            plan = self.group.plan()
-            self.endState = plan.joint_trajectory.points[len(plan.joint_trajectory.points) - 1].positions
-            self.group.execute(plan, wait=False)
-
-""" 
-#def __init__(self, move=True, waitForExecution=True, group="RightArm", watchdog=15):
-    self.group = MoveGroupCommander(group)
-#    curState = self.group.get_current_joint_values()
-    self.group.set_pose_target(userdata.target)
-#    self.group.set_position_target(xyz)
-#    self.group.set_named_target(userdata.target)
-    plan = self.group.plan()
-    self.endState = plan.joint_trajectory.points[len(plan.joint_trajectory.points) - 1].positions
-    self.group.execute(plan, wait=False)
-#self.group.stop()
-#self.group.stop()
-"""
+        p1 = Popen(split("python3 ../MTI805/openpifpafwebdemo/server.py"), stdout=PIPE)
+        p2 = Popen(split("python2 pifpafToMoveit.py"), stdin=p1.stdout)
 
 
 if __name__ == "__main__":
--- src/pifpafToMoveit.py
+++ src/pifpafToMoveit.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # From https://stackoverflow.com/a/55517239/7243716 or https://stackoverflow.com/questions/35176451/python-code-to-calculate-angle-between-three-point-using-their-3d-coordinates#35178910
 import numpy as np
-import vg
 import math
 from geometry_msgs.msg import Pose
+from moveit_commander import MoveGroupCommander
+from tf import transformations
 
 # From https://stackoverflow.com/a/17796482/7243716 or https://stackoverflow.com/questions/17796446/convert-a-list-to-a-string-and-back#17796482
 from ast import literal_eval
@@ -13,63 +16,68 @@ import sys
 import re
 
 
-def main():
-    # Nombre de cycle nécessaire pour envoyer un message (1 msg/4 cycles)
-    PERIODICITY = 4
-    cycles_without_sending = 0  # variable indiquant le cycle actuel
+def calculatePose(givenArray):
+    """
+     # Nombre de points : 17 [0-16]
+                        # 0 : nez
+                        # 1 : oeil gauche
+                        # 2 : oeil droit
+                        # 3 : oreille gauche
+                        # 4 : oreille droite
+                        # 5 : epaule gauche
+                        # 6 : epaule droite
+                        # 7 : coude gauche
+                        # 8 : coude droit
+                        # 9 : main gauche
+                        # 10 : main droite
+                        # 11 : hanche gauche
+                        # 12 : hanche droite
+                        # 13 : genoux gauche
+                        # 14 : genoux droit
+                        # 15 : pied gauche
+                        # 16 : pied droit
+    """
+    # Extrait les coordonnees des 3 joints du bras gauche
+    # a, b, c = np.array((givenArray[6],givenArray[8],givenArray[10]))
+    # Extrait les coordonnees des 3 joints du bras droit
+    # epaule gauche
+    # 5 : epaule gauche
+    a = np.array((givenArray[5]))
+    # 7 : coude gauche
+    b = np.array((givenArray[7]))
+    # 9 : poignet gauche
+    c = np.array((givenArray[9]))
+    # Vectors
+    ab = b - a
+    bc = c - b
+    # Get position and orientation of right wrist
+    x,y,z = c
+    dist_x_y = (x**2+y**2)**0.5
+    roll = 0
+    pitch = math.atan2(y,x)
+    yaw = math.atan2(z,dist_x_y)
+    orientation = transformations.quaternion_from_euler(roll,pitch,yaw)
+    return Pose(c,orientation)
 
-    # précompilation du regex, indication des motifs de mots à enlever
-    regex = re.compile(r"prepro.*")
-    last_angles = []
-    while True:
-        # Lis ligne dans le pipelin stdin
-        line = sys.stdin.readline()
-        # enlève les motifs de mot prédeterminés de la string par substitution
-        line = regex.sub("", line)
-
-        # si on a plus d'entrée
-        if not line:
-            # on sort de la boucle while (fin du programme)
-            break
-
-        # si on a que des espaces blanc
-        if line.isspace() or "[]" in line:
-            # on passe à la prochaine itération
-            continue
-
-        # on extirpe seulement la liste les coordonnées de la string
-        givenArray = literal_eval(line)[0]['coordinates']
-
-        if len(givenArray) < 3:
-            sys.stdout.write(repr(givenArray))
-            sys.stdout.flush()
-            continue
-
-        angle1, angle2 = calculateAngles(givenArray)
-
-        # met a jour la liste de tous les angles calculés depuis la dernière période
-        last_angles = update_liste_angles(last_angles, [angle1, angle2], PERIODICITY)
-
-        sendAccordingToPeriodicity(PERIODICITY, cycles_without_sending, last_angles)
 
 
 def sendAccordingToPeriodicity(periodicity, cycles_without_sending, last_angles):
-    # on incrémente le compteur de cycle sans message
+    # on incremente le compteur de cycle sans message
     cycles_without_sending += 1
-    # si le compteur de cycle correspond à n période
+    # si le compteur de cycle correspond à n periode
     if cycles_without_sending % periodicity == 0:
-        # on remet le compte à 0 pour ne pas dépasser la valeur max du type
+        # on remet le compte à 0 pour ne pas depasser la valeur max du type
         cycles_without_sending = 0
 
         output_str = reprAngles(last_angles)
 
         # envoi l'information au prochain dans std out
         sys.stdout.write(output_str)
-        # force l'écriture en .vitant le buffering (pratique pour python2.7)
+        # force l'ecriture en .vitant le buffering (pratique pour python2.7)
         sys.stdout.flush()
 
 
-def reprAngles(last_angles) -> str:
+def reprAngles(last_angles) :
     # calcule les angles avec la moyenne mobile
     angles_to_send = moyenne_mobile(last_angles)
     output_str = str(angles_to_send[0]) + " " + str(angles_to_send[1]) + "\n"
@@ -77,9 +85,9 @@ def reprAngles(last_angles) -> str:
 
 
 def calculateAngles(givenArray):
-    # Extrait les coordonnées des 3 joints du bras gauche
+    # Extrait les coordonnees des 3 joints du bras gauche
     # a, b, c = np.array((givenArray[6],givenArray[8],givenArray[10]))
-    # Extrait les coordonnées des 3 joints du bras droit
+    # Extrait les coordonnees des 3 joints du bras droit
     a, b, c = np.array((givenArray[5], givenArray[7], givenArray[9]))
     # Vectors
     ab = b - a
@@ -104,30 +112,30 @@ Pose(pos, orientation)
 """
 
 # Garde une liste a jour avec <les max_size> derniers elements
-def update_liste_angles(liste_angles, new_elements, max_size):
-    # on verifie si les angle ne sont pas nan
-    for i, angle in enumerate(new_elements):
-        # si l<angle est nan
-        if math.isnan(angle):
+def update_liste_values(liste_values, new_elements, max_size):
+    # on verifie si les values ne sont pas nan
+    for i, value in enumerate(new_elements):
+        # si la value est nan
+        if math.isnan(value):
             # si la liste n'est pas vide
-            # print("angle ", i, "  was nan: ",angle)
-            if len(liste_angles) > 1:
+            # print("value ", i, "  was nan: ",value)
+            if len(liste_values) > 1:
                 # on prend sa derniere valeur connue
-                new_elements[i] = liste_angles[-1][i]
+                new_elements[i] = liste_values[-1][i]
             # sinon on met les valeurs a 0
             else:
                 new_elements[i] = 0
 
-    liste_angles.append(new_elements)
-    while len(liste_angles) > max_size:
-        liste_angles.pop(0)
-    return liste_angles
+    liste_values.append(new_elements)
+    while len(liste_values) > max_size:
+        liste_values.pop(0)
+    return liste_values
 
 
 def moyenne_mobile(list_values):
     # etabli le diviseur par le nombre de liste de values
     diviseur = len(list_values)
-    # établi le nombre de values dans une liste de value
+    # etabli le nombre de values dans une liste de value
     dim = len(list_values[0])
     # tableau contenant les moyennes des values
     final_values = []
@@ -150,5 +158,69 @@ def moyenne_mobile(list_values):
 
     return final_values
 
+
+def sendCommandToMoveit(PERIODICITY, cycles_without_sending, last_value, latest_state={}):
+#    self.group.set_pose_target(userdata.target)
+    latest_state.group.set_position_target(last_value)
+    plan = latest_state.group.plan()
+    latest_state.endState = plan.joint_trajectory.points[len(plan.joint_trajectory.points) - 1].positions
+    latest_state.group.execute(plan, wait=False)
+    return latest_state
+
+
+def main():
+    # Nombre de cycle necessaire pour envoyer un message (1 msg/4 cycles)
+    PERIODICITY = 4
+    cycles_without_sending = 0  # variable indiquant le cycle actuel
+
+    # precompilation du regex, indication des motifs de mots à enlever
+    regex = re.compile(r"prepro.*")
+    last_value = []
+    latestState = {'group':MoveGroupCommander("RightArm")}
+    while True:
+        # Lis ligne dans le pipelin stdin
+        line = sys.stdin.readline()
+        # enlève les motifs de mot predetermines de la string par substitution
+        line = regex.sub("", line)
+
+        # si on a plus d'entree
+        if not line:
+            # on sort de la boucle while (fin du programme)
+            break
+
+        # si on a que des espaces blanc
+        if line.isspace() or "[]" in line:
+            # on passe à la prochaine iteration
+            continue
+
+        # on extirpe seulement la liste les coordonnees de la string
+        givenArray = literal_eval(line)[0]['coordinates']
+
+        if len(givenArray) < 3:
+            sys.stdout.write(repr(givenArray))
+            sys.stdout.flush()
+            continue
+
+#        pose = calculatePose(givenArray)
+        last_value = np.array((givenArray[9]))
+        last_value[1] = -last_value[1]
+
+        # met a jour la liste de toutes les poses calcules depuis la dernière periode
+#        last_value = update_liste_values(last_value, pos, PERIODICITY)
+
+#        sendAccordingToPeriodicity(PERIODICITY, cycles_without_sending, last_angles)
+        latestState = sendCommandToMoveit(PERIODICITY, cycles_without_sending, last_value, latestState)
+""" 
+#def __init__(self, move=True, waitForExecution=True, group="RightArm", watchdog=15):
+    self.group = MoveGroupCommander(group)
+#    curState = self.group.get_current_joint_values()
+    self.group.set_pose_target(userdata.target)
+#    self.group.set_position_target(xyz)
+    plan = self.group.plan()
+    self.endState = plan.joint_trajectory.points[len(plan.joint_trajectory.points) - 1].positions
+    self.group.execute(plan, wait=False)
+#self.group.stop()
+#self.group.stop()
+"""
 
 main()
